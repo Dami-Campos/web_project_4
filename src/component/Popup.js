04414878f53c3bf5e5d73de5513b8ed35bb75a8b
Dami-Campos <105475538+Dami-Campos@users.noreply.github.com>
2023-02-24 14:05:50 -0600

    commit de githubpages

--- src/component/Popup.js
+++ src/component/Popup.js
@@ -1,5 +1,3 @@
-import { popupCloseButton } from "./utils/utils.js"
-
 export default class Popup {
     constructor(popupSelector) {
       this._popupElement = document.querySelector(popupSelector);
